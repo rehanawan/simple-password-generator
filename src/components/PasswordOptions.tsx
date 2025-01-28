8b56b5d808dd38eca68faf6fa2eb9cc32ecef0a5
David <david.jaeren@gmail.com>
2025-01-28 09:26:00 +0000

    Added prettier to make the code consistent. Updated useEffect hook deps

--- src/components/PasswordOptions.tsx
+++ src/components/PasswordOptions.tsx
@@ -1,59 +1,59 @@
 import React from 'react';
 
 interface PasswordOptionsProps {
-    length: number;
-    setLength: (length: number) => void;
-    includeSpecialCharacters: boolean;
-    setIncludeSpecialCharacters: (include: boolean) => void;
-    includeNumbers: boolean;
-    setIncludeNumbers: (include: boolean) => void;
-
+  length: number;
+  setLength: (length: number) => void;
+  includeSpecialCharacters: boolean;
+  setIncludeSpecialCharacters: (include: boolean) => void;
+  includeNumbers: boolean;
+  setIncludeNumbers: (include: boolean) => void;
 }
 
 export const PasswordOptions: React.FC<PasswordOptionsProps> = ({
-                                                                    length,
-                                                                    setLength,
-                                                                    includeSpecialCharacters,
-                                                                    setIncludeSpecialCharacters,
-                                                                    includeNumbers,
-                                                                    setIncludeNumbers,
-                                                                }) => {
-
-
-    return (
-        <div className="grid items-center gap-2 py-4">
-            <div className="flex items-center gap-2">
-                <label className={"flex gap-2 w-full"}>Length: {length}
-                </label>
-                <input
-                    className="w-full"
-                    type="range"
-                    min={8}
-                    max={20}
-                    value={length}
-                    onChange={(e) => setLength(parseInt(e.target.value))}
-                />
-            </div>
-            <div className="flex items-center">
-                <label className={"flex items-center gap-2 w-full"}>
-                <input
-                    className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                    type="checkbox"
-                    checked={includeSpecialCharacters}
-                    onChange={() => setIncludeSpecialCharacters(!includeSpecialCharacters)}
-                />
-                    Special Characters</label>
-            </div>
-            <div className="flex items-center">
-                <label className={"flex items-center gap-2"}>
-                <input
-                    className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                    type="checkbox"
-                    checked={includeNumbers}
-                    onChange={() => setIncludeNumbers(!includeNumbers)}
-                />
-                    Numbers</label>
-            </div>
-        </div>
-    );
+  length,
+  setLength,
+  includeSpecialCharacters,
+  setIncludeSpecialCharacters,
+  includeNumbers,
+  setIncludeNumbers,
+}) => {
+  return (
+    <div className='grid items-center gap-2 py-4'>
+      <div className='flex items-center gap-2'>
+        <label className={'flex gap-2 w-full'}>Length: {length}</label>
+        <input
+          className='w-full'
+          type='range'
+          min={8}
+          max={20}
+          value={length}
+          onChange={(e) => setLength(parseInt(e.target.value))}
+        />
+      </div>
+      <div className='flex items-center'>
+        <label className={'flex items-center gap-2 w-full'}>
+          <input
+            className='h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600'
+            type='checkbox'
+            checked={includeSpecialCharacters}
+            onChange={() =>
+              setIncludeSpecialCharacters(!includeSpecialCharacters)
+            }
+          />
+          Special Characters
+        </label>
+      </div>
+      <div className='flex items-center'>
+        <label className={'flex items-center gap-2'}>
+          <input
+            className='h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600'
+            type='checkbox'
+            checked={includeNumbers}
+            onChange={() => setIncludeNumbers(!includeNumbers)}
+          />
+          Numbers
+        </label>
+      </div>
+    </div>
+  );
 };
--- src/paracet/utils.ts
+++ src/paracet/utils.ts
@@ -1,24 +1,24 @@
-import {GeneratePasswordOptions} from "./types.ts";
+import { GeneratePasswordOptions } from './types.ts';
 export const getRandomCharacter = (characterPool: string): string => {
-    return characterPool.charAt(Math.floor(Math.random() * characterPool.length));
+  return characterPool.charAt(Math.floor(Math.random() * characterPool.length));
 };
 export const generateRandomPassword = ({
-                                           length,
-                                           includeSpecialCharacters,
-                                           includeNumbers,
-                                       }: GeneratePasswordOptions): string => {
-    let characterPool = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzåøæÅØÆ";
-    const digits = "0123456789";
-    const specialCharacters = "!@#$%^&*()_+-=";
-    let password = "";
+  length,
+  includeSpecialCharacters,
+  includeNumbers,
+}: GeneratePasswordOptions): string => {
+  let characterPool =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzåøæÅØÆ';
+  const digits = '0123456789';
+  const specialCharacters = '!@#$%^&*()_+-=';
+  let password = '';
 
-    if (includeNumbers) characterPool += digits;
-    if (includeSpecialCharacters) characterPool += specialCharacters;
+  if (includeNumbers) characterPool += digits;
+  if (includeSpecialCharacters) characterPool += specialCharacters;
 
+  for (let i = 0; i < length; i++) {
+    password += getRandomCharacter(characterPool);
+  }
 
-        for (let i = 0; i < length; i++) {
-            password += getRandomCharacter(characterPool);
-        }
-
-    return password;
+  return password;
 };
